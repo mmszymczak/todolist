58274ffcea47f3d8b362f8a8b6c2e2102780693c
mmszymczak <person.szymczak@gmail.com>
2016-03-11 18:09:37 +0100

    fixes and created array of titles in local storage

--- js/app.js
+++ js/app.js
@@ -2,30 +2,50 @@
 	'use strict';
 
 	var item = document.querySelector('.new-todo'),
-		list = document.querySelector('.todo-list');
+		list = document.querySelector('.todo-list'),
+		titlesFromLocalStorage = [];
 
-	function createListItem(){
+//	if local storage exists then get elements from there and assign to local array
+	if(window.localStorage.titles){
+		titlesFromLocalStorage = JSON.parse(window.localStorage.titles); 
+	}
+//	will create new li element and appends to the end of ul
+	function createListItem(title){
 		var li = document.createElement('li');
+		
+	    li.innerHTML = '<div class="view">'
+		+				'<input class="toggle" type="checkbox">'
+		+				'<label>'+ title +'</label>'
+		+				'<button class="turn-important"></button>'
+		+				'<button class="destroy"></button>'
+		+				'</div>';
 
-		if(item.value.trim() === ''){
-			item.value = '';
-			return;
-		}else{
-		    li.innerHTML = '<div class="view">'
-			+				'<input class="toggle" type="checkbox">'
-			+				'<label>'+ item.value +'</label>'
-			+				'<button class="turn-important"></button>'
-			+				'<button class="destroy"></button>'
-			+				'</div>';
-
-			list.appendChild(li);
-			store();
-		}
-		item.value = '';
+		list.appendChild(li);
+	}
+//	if Enter pressed then get title(value) from input, creating element and adding to local storage
+	function addElement(e) {
+	    if(e.which === 13) {
+	    	if(item.value.trim() === ''){
+				item.value = ''; 
+				return;
+			}
+			createListItem(item.value);
+			addTitleToLocalStorage(item.value);
+			item.value = '';	 
+	    }
 	}
 
-	function keyPress(e) {
-	    if(e.which === 13) { createListItem(); }
+	function addTitleToLocalStorage(title){
+		titlesFromLocalStorage.push(title);
+		window.localStorage.titles = JSON.stringify(titlesFromLocalStorage);
+	}
+
+	function removeTitleFromLocalStorage(title){
+		var index = titlesFromLocalStorage.indexOf(title);
+		if(index > -1){
+			titlesFromLocalStorage.splice(index,1);
+		}
+		window.localStorage.titles = JSON.stringify(titlesFromLocalStorage);
 	}
 
 	window.findParent = function (element, tagName) {
@@ -38,14 +58,15 @@
 		return window.findParent(element.parentNode, tagName);
 	};
 
-	function store(){
+/*	function store(){
 		window.localStorage.myitems = list.innerHTML;
 	}
-
-	function retrieve(){
+*/
+/*	function retrieve(){
 		list.innerHTML = window.localStorage.myitems || '';
-	}
+	}*/
 
+//	will trigger list item between classes completed and empty
 	function editInput(event){
 		var t = event.target,
 			parent = findParent(t,'li'),
@@ -63,17 +84,19 @@
 				t.removeAttribute('checked');
 			}
 		}
-		store();
+		//store();
 	}
-
+// 	if button.important then toggle class="important" else if button.destroy then remove item from ul and local storage
 	function editButton(event){
 		var t = event.target,
 			parent = findParent(t,'li'),
 			parentClass = parent.classList;
+		var title = parent.getElementsByTagName('label')[0].innerHTML;
 
 		if(t.tagName === 'BUTTON'){
 			if(t.classList.contains('destroy')){
 				findParent(t,'ul').removeChild(parent);
+				removeTitleFromLocalStorage(title);
 			}
 			if(t.classList.contains('turn-important')){
 				if(!parentClass.contains('important')){
@@ -88,9 +111,9 @@
 				}
 			}
 		}
-		store();
+		//store();
 	}
-
+//	will create new editable input which has the same value as before with focus, after Enter pressed adding changes to existing label
 	function editLabel(event){
 		var t = event.target,
 			parent = findParent(t,'li'),
@@ -107,7 +130,7 @@
 					t.innerHTML = input.value;
 					parent.removeChild(input);
 					parent.classList.remove('editing');
-					store();
+					//store();
 				}
 			});
 		}
@@ -116,14 +139,17 @@
 	function toggleEvents(event){
 		var t = event.target.tagName;
 
-		//if(t === 'LABEL'){ editLabel(event); }
+		list.addEventListener('dblclick', editLabel);
 		if(t === 'BUTTON'){ editButton(event); }
 		if(t === 'INPUT'){ editInput(event); }
 	}
 
-	item.addEventListener('keypress', keyPress);
-	list.addEventListener('click', toggleEvents);
-	list.addEventListener('dblclick', editLabel);
+//	will create all list elements based on titles from local storage
+	titlesFromLocalStorage.forEach(function(title){
+		createListItem(title);
+	});
 
-	retrieve();
+	item.addEventListener('keypress', addElement);
+	list.addEventListener('click', toggleEvents);
+
 })();
